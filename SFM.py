910208ac904d6e3d2059ea224746372fc2aa733d
James Carlson <jxxcarlson@mac.com>
2011-03-27 06:03:29 -0600

    Comments

--- SFM.py
+++ SFM.py
@@ -1,25 +1,47 @@
+#########################################################
+#
+# File: SFM.py
+# SFM = solfa machine
+#
+# Authors: James Carlson and Hakon Hallgrimur
+# Date:    March 27, 2011
+#
+# Purpose: transform a string of augmented solfa symbols
+#          into a string of tuple, where a tuple is
+#          (frequency, duration, amplitude, decay)
+#
+# Example: from SFM import SFM
+#          S = SFM()
+#          S.input = "q do re mi"
+#          print S.tuples()
+#          print "Total duration:", S.totalDuration
+#          print "Total beats:", S.currentBeat
+#          print "Maximum amplitude:", S.maximumAmplitude
+#
+##########################################################
+
 import math
-from dynamics import *
 from parse import splitToken
 from note import Note
 from rhythm import Rhythm
+from dynamics import Dynamics
 
 class SFM(object):
 
-  # registers
+  # tempo registers
   tempo = 60
   transpositionSemitones = 0
   currentBeatValue = 0
   currentBeat = 0
   totalDuration = 0.0
   
-  # dynamics
+  # dynamics registeres
   crescendoSpeed = 1.1
   currentCrescendoSpeed = crescendoSpeed
   crescendoBeatsRemaining = 0.0
   maximumAmplitude = 0.0
 
-  # registers for tuples
+  # tuple registers
   duration = 1.0/tempo
   amplitude = 1.0
   decay = 1.0
@@ -29,11 +51,14 @@ class SFM(object):
   
   #########################################################
   
+  # initialize Note, Rhythm, and Dynamics objects
   def __init__(self):
     self.note = Note()
     self.rhythm = Rhythm()
     self.dynamics = Dynamics()
-    
+
+  # return tuple as string given frequency,
+  # duration, decay, and amplitude
   def _tuple(self, freq, duration):
     output = `freq`
     output += " "+`duration`
@@ -41,7 +66,8 @@ class SFM(object):
     output += " "+`self.decay`
     return output
   
-      
+  # return tuple as string from frequency of token
+  # and root and suffix of token
   def tuple(self, freq, root, suffix):
     if suffix.find(",") > -1:
           thisDuration = self.duration*(1 - self.rhythm.breath)
@@ -52,16 +78,17 @@ class SFM(object):
           output = self._tuple(freq, self.duration)
     return output
     
-    
+  # tuples: returns a string of tuples from input = solfa text
   def tuples(self):
     tokens = self.input.split(" ")
+    output = ""
     for token in tokens:
       if self.note.isNote(token):
         if self.crescendoBeatsRemaining > 0:
           self.amplitude = self.amplitude*self.currentCrescendoSpeed
           self.crescendoBeatsRemaining -= self.currentBeatValue
         freq, root, suffix = self.note.freq(token, self.transpositionSemitones)
-        print self.tuple(freq, root, suffix)
+        output += self.tuple(freq, root, suffix) + "\n"
         
         # summary data
         self.totalDuration += self.duration
@@ -72,12 +99,12 @@ class SFM(object):
       else:
         ops = token.split(":")
         ops = filter(lambda x: len(x) > 0, ops)
-        print "cmd:", ops
+        # print "cmd:", ops
         cmd = ops[0]
         
         # if cmd is a rhythm symbol, change value of duration register
         if self.rhythm.isRhythmOp(cmd):
-          self.currentBeatValue, self.duration = self.rhythm.value(cmd, self.tempo) 
+          self.currentBeatValue, self.duration = self.rhythm.value(cmd, self.tempo)
     
         # if cmd is a tempo command, change value of the tempo register
         if self.rhythm.isTempoOp(cmd):
@@ -90,12 +117,16 @@ class SFM(object):
         # if cmd is a dynamics command, change value of the amplitude register
         if self.dynamics.isDynamicsConstant(cmd):
           self.amplitude = self.dynamics.value[cmd]
+          
+        # crescendo and decrescendo
         if cmd == "crescendo" or cmd == "cresc":
           self.crescendoBeatsRemaining = float(ops[1])
           self.currentCrescendoSpeed = self.crescendoSpeed
         if cmd == "decrescendo" or cmd == "decresc":
           self.crescendoBeatsRemaining = float(ops[1])
           self.currentCrescendoSpeed = 1.0/self.crescendoSpeed
+          
+    return output
 
  
 
