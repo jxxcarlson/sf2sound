c2250c0d105a5c997dcb7bb7eb4c5ac605303d68
James Carlson <jxxcarlson@mac.com>
2011-04-13 19:08:00 -0400

    Tested and fixed setup.sh.  Initial commit of dictation.txt/

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #  INSTALL_DIR=~/Dropbox/bin\
-INSTALL_DIR=/Applications/sf2a/
+INSTALL_DIR=/Applications/sf2a
 BIN_DIR=~/Dropbox/bin
 
 case $1 in 
@@ -15,42 +15,43 @@ case $1 in
 	fi
 
 	gcc quad2samp.c -o quad2samp
-	mv quad2samp $INSTALL_DIR
+	mv quad2samp $INSTALL_DIR/
 	gcc mix.c -o mix
-	mv mix $INSTALL_DIR
-	cp text2sf $INSTALL_DIR
+	mv mix $INSTALL_DIR/
+	cp text2sf $INSTALL_DIR/
 
-	cp driver.py $INSTALL_DIR
-	cp sf2a.py $INSTALL_DIR 
-	cp SFM.py $INSTALL_DIR
+	cp driver.py $INSTALL_DIR/
+	cp sf2a.py $INSTALL_DIR/ 
+	cp SFM.py $INSTALL_DIR/
 
-	cp note.py $INSTALL_DIR
-	cp scales.py $INSTALL_DIR
-	cp rhythm.py $INSTALL_DIR
-	cp dynamics.py $INSTALL_DIR
+	cp note.py $INSTALL_DIR/
+	cp scales.py $INSTALL_DIR/
+	cp rhythm.py $INSTALL_DIR/
+	cp dynamics.py $INSTALL_DIR/
 
-	cp parse.py $INSTALL_DIR
-	cp comment.py $INSTALL_DIR
-	cp stringUtil.py $INSTALL_DIR
+	cp parse.py $INSTALL_DIR/
+	cp comment.py $INSTALL_DIR/
+	cp stringUtil.py $INSTALL_DIR/
 
-	cp stack.py $INSTALL_DIR
-	cp ring.py $INSTALL_DIR
-        cp melody.py $INSTALL_DIR
+	cp stack.py $INSTALL_DIR/
+	cp ring.py $INSTALL_DIR/
+        cp melody.py $INSTALL_DIR/
 
-	cp listUtil.py $INSTALL_DIR
+	cp listUtil.py $INSTALL_DIR/
 	cp ui.py $INSTALL_DIR/sf2a 
 
         cp dict.py $INSTALL_DIR/dict
-        cp script $INSTALL_DIR
-	cp element5 $INSTALL_DIR
-	cp element6 $INSTALL_DIR 
-	cp style.css $INSTALL_DIR
+        cp script $INSTALL_DIR/
+	cp element5 $INSTALL_DIR/
+	cp element6 $INSTALL_DIR/ 
+	cp style.css $INSTALL_DIR/
+        cp dictation.txt $INSTALL_DIR/
 
 	ln -s $INSTALL_DIR/sf2a $BIN_DIR/sf2a
-	chmod u+x $BIN_DIR/sf2a
-
 	ln -s $INSTALL_DIR/dict $BIN_DIR/dict
-	chmod u+x $BIN_DIR/dict ;;
+
+        chmod u+x $INSTALL_DIR/dict $INSTALL_DIR/sf2a  
+        chown $2 $INSTALL_DIR/* ;;
 
   -test)
 	echo
